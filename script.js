37824d7078db3f9ec5e20ba3c047acee8d900cc2
didiu07 <162641508+didiu07@users.noreply.github.com>
2024-10-02 19:20:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [
+        enunciado: " bora bill ou luva de pedreiro?
+      alternativas: [
             {
                 texto: "Isso é assustador!",
                 afirmacao: ["No início ficou com medo do que essa tecnologia pode fazer. ",
